3ef3a08413fb50aeb845f2b3e8880adcc58c29b0
valehash <newlechim@gmail.com>
2024-03-09 17:49:23 +0100

    user seems i ned to save

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""The user class"""
 from models.base_model import BaseModel
 
 class User(BaseModel):
+    """The user class"""
     email = ''
     password = ''
     first_name = ''
